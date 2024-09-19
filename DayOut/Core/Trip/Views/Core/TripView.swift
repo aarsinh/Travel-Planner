801483d51ffc3afaaec50991d7cc2e2dca8b3d7d
aarsinh <148481261+aarsinh@users.noreply.github.com>
2024-09-19 22:51:44 +0530

    improved trip deletion

--- DayOut/Core/Trip/Views/Core/TripView.swift
+++ DayOut/Core/Trip/Views/Core/TripView.swift
@@ -13,8 +13,10 @@ struct TripView: View {
     @Environment(\.dismiss) var dismiss
     @EnvironmentObject var viewModel: TripViewModel
     @State private var addingPlans = false
+    @State private var showDeleteAlert = false
     @State private var tripPlans: [Plan] = []
     @State private var tripId: String = ""
+    @State private var saveState: SaveState = .idle
 
     private var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
@@ -39,132 +41,148 @@ struct TripView: View {
     }
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                ScrollView {
-                    VStack(alignment: .leading, spacing: 16) {
-                        HStack {
-                            Text(selectedTrip.startDate != selectedTrip.endDate ? "\(selectedTrip.startDate) - \(selectedTrip.endDate)" : "\(selectedTrip.startDate)")
-                                .font(.title3)
-                            Spacer()
-                        }
-                        .padding(.horizontal)
-
-                        if selectedTrip.plans.isEmpty {
-                            VStack {
-                                Text("No plans added yet")
-                                    .bold()
-                                Text("Add Plans here")
-                                Button {
-                                    addingPlans = true
-                                } label: {
-                                    ZStack {
-                                        RoundedRectangle(cornerRadius: 7)
-                                            .frame(height: 50)
-                                            .padding(.horizontal)
-                                        Text("Add Plans")
-                                            .foregroundStyle(.white)
-                                    }
-                                }
+        ZStack {
+            NavigationStack {
+                VStack {
+                    ScrollView {
+                        VStack(alignment: .leading, spacing: 16) {
+                            HStack {
+                                Text(selectedTrip.startDate != selectedTrip.endDate ? "\(selectedTrip.startDate) - \(selectedTrip.endDate)" : "\(selectedTrip.startDate)")
+                                    .font(.title3)
+                                Spacer()
                             }
-                            .padding(.top, 30)
-                        } else {
-                            ForEach(sortedDates, id: \.self) { date in
-                                VStack(alignment: .leading) {
-                                    
-                                    ZStack(alignment: .center) {
-                                        Rectangle()
-                                            .frame(height: 35)
-                                            .foregroundStyle(.secondary.opacity(0.4))
-                                        
-                                        Text("\(date)")
-                                            .font(.system(size: 14))
-                                            .bold()
-                                    }
-                                    .padding(.top)
-                                
-                                    ForEach(groupedPlans[date] ?? []) { plan in
-                                        if plan.type == "Flight" {
-                                            NavigationLink(destination: FlightDetailView(flight: plan, tripId: tripId)) {
-                                                PlanDetail(plan: plan)
-                                            }
-                                        } else {
-                                            NavigationLink(destination: PlanDetailView(plan: plan, tripId: tripId)) {
-                                                PlanDetail(plan: plan)
-                                            }
+                            .padding(.horizontal)
+                            
+                            if selectedTrip.plans.isEmpty {
+                                VStack {
+                                    Text("No plans added yet")
+                                        .bold()
+                                    Text("Add Plans here")
+                                    Button {
+                                        addingPlans = true
+                                    } label: {
+                                        ZStack {
+                                            RoundedRectangle(cornerRadius: 7)
+                                                .frame(height: 50)
+                                                .padding(.horizontal)
+                                            Text("Add Plans")
+                                                .foregroundStyle(.white)
                                         }
                                     }
-                                    .buttonStyle(.plain)
+                                }
+                                .padding(.top, 30)
+                            } else {
+                                ForEach(sortedDates, id: \.self) { date in
+                                    VStack(alignment: .leading) {
+                                        
+                                        ZStack(alignment: .center) {
+                                            Rectangle()
+                                                .frame(height: 35)
+                                                .foregroundStyle(.secondary.opacity(0.4))
+                                            
+                                            Text("\(date)")
+                                                .font(.system(size: 14))
+                                                .bold()
+                                        }
+                                        .padding(.top)
+                                        
+                                        ForEach(groupedPlans[date] ?? []) { plan in
+                                            if plan.type == "Flight" {
+                                                NavigationLink(destination: FlightDetailView(flight: plan, tripId: tripId)) {
+                                                    PlanDetail(plan: plan)
+                                                }
+                                            } else {
+                                                NavigationLink(destination: PlanDetailView(plan: plan, tripId: tripId)) {
+                                                    PlanDetail(plan: plan)
+                                                }
+                                            }
+                                        }
+                                        .buttonStyle(.plain)
+                                    }
+                                }
+                            }
+                        }
+                        .onAppear {
+                            Task {
+                                if let tripId = try await viewModel.fetchTripId(by: selectedTrip.id) {
+                                    self.tripId = tripId
+                                    selectedTrip = try await viewModel.refreshTrip(id: tripId)
                                 }
                             }
                         }
                     }
-                    .onAppear {
+                    .fullScreenCover(isPresented: $addingPlans) {
+                        AddPlanView(trip: selectedTrip)
+                            .onDisappear {
+                                Task {
+                                    if let tripId = try await viewModel.fetchTripId(by: selectedTrip.id) {
+                                        selectedTrip = try await viewModel.refreshTrip(id: tripId)
+                                        tripPlans = selectedTrip.plans
+                                    }
+                                }
+                            }
+                    }
+                }
+                
+                .navigationTitle(selectedTrip.tripName)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Menu {
+                            NavigationLink(destination: MapView(plans: selectedTrip.plans)) {
+                                Label("Map View", systemImage: "map")
+                            }
+                            .disabled(selectedTrip.plans.isEmpty)
+                            
+                            Button(role: .destructive) {
+                                showDeleteAlert = true
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                            
+                        } label: {
+                            Image(systemName: "ellipsis.circle")
+                        }
+                    }
+                }
+                .alert("Are you sure?", isPresented: $showDeleteAlert) {
+                    Button("Cancel", role: .cancel) { }
+                    Button("Delete", role: .destructive) {
+                        saveState = .saving
                         Task {
                             if let tripId = try await viewModel.fetchTripId(by: selectedTrip.id) {
-                                self.tripId = tripId
-                                selectedTrip = try await viewModel.refreshTrip(id: tripId)
+                                try await viewModel.deleteTrip(withId: tripId)
+                                dismiss()
+                                saveState = .saved
                             }
                         }
                     }
+                } message: {
+                    Text("Deleting this trip will permanently remove it from your trips.")
                 }
-                .fullScreenCover(isPresented: $addingPlans) {
-                    AddPlanView(trip: selectedTrip)
-                        .onDisappear {
-                            Task {
-                                if let tripId = try await viewModel.fetchTripId(by: selectedTrip.id) {
-                                    selectedTrip = try await viewModel.refreshTrip(id: tripId)
-                                    tripPlans = selectedTrip.plans
-                                }
+                .overlay(
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Button {
+                                addingPlans = true
+                            } label: {
+                                Image(systemName: "plus")
+                                    .foregroundStyle(.white)
+                                    .font(.system(size: 24, weight: .bold))
+                                    .frame(width: 50, height: 50)
+                                    .background(.blue)
+                                    .clipShape(.circle)
                             }
+                            .padding()
                         }
-                }
+                    }
+                )
             }
             
-            .navigationTitle(selectedTrip.tripName)
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Menu {
-                        NavigationLink(destination: MapView(plans: selectedTrip.plans)) {
-                            Label("Map View", systemImage: "map")
-                        }
-                        .disabled(selectedTrip.plans.isEmpty)
-                        
-                        Button(role: .destructive) {
-                            Task {
-                                if let tripId = try await viewModel.fetchTripId(by: selectedTrip.id) {
-                                    try await viewModel.deleteTrip(withId: tripId)
-                                    dismiss()
-                                }
-                            }
-                        } label: {
-                            Label("Delete", systemImage: "trash")
-                        }
-                        
-                    } label: {
-                        Image(systemName: "ellipsis.circle")
-                    }
-                }
+            if saveState == .saving {
+                SavingView(text: "Deleting trip...")
             }
-            .overlay(
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Button {
-                            addingPlans = true
-                        } label: {
-                            Image(systemName: "plus")
-                                .foregroundStyle(.white)
-                                .font(.system(size: 24, weight: .bold))
-                                .frame(width: 50, height: 50)
-                                .background(.blue)
-                                .clipShape(.circle)
-                        }
-                        .padding()
-                    }
-                }
-            )
         }
     }
 }
